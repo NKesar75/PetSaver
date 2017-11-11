a6fe004278ea0c2604a1139c019258fda745f55b
On Lam <olam@fullsail.edu>
2017-11-11 17:40:07 -0500

    PUsh

---
+++ HackAthon2017/app/src/main/java/domain/hackathon/hackathon2017/ShelterInfo.java
@@ -0,0 +1,13 @@
+package domain.hackathon.hackathon2017;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ShelterInfo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_shelter_info);
+    }
+}
