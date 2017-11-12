d021d7226a73929f4c9c2ab371deb1d0ab01925e
Raj Chandan <RPChandan@fullsail.edu>
2017-11-12 05:34:10 -0500

    dsdcdsc

--- HackAthon2017/app/src/main/java/domain/hackathon/hackathon2017/Fav_Desc.java
+++ HackAthon2017/app/src/main/java/domain/hackathon/hackathon2017/Fav_Desc.java
@@ -3,6 +3,7 @@ package domain.hackathon.hackathon2017;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -69,5 +70,19 @@ public class Fav_Desc extends AppCompatActivity {
             }
         });
 
+        if(getSupportActionBar()!= null)
+        {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            getSupportActionBar().setDisplayShowHomeEnabled(true);
+        }
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(item.getItemId()==android.R.id.home)
+            finish();
+
+        return super.onOptionsItemSelected(item);
     }
 }
