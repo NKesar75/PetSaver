bf1ba7c25bbce9a54ab183624b8f0e917b26abed
Nilesh Kesar <NKesar@fullsail.edu>
2017-11-21 11:12:50 -0500

    dsafasdf

---
+++ HackAthon2017/app/src/main/java/domain/hackathon/hackathon2017/InvaildPage.java
@@ -0,0 +1,46 @@
+package domain.hackathon.hackathon2017;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.EditText;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import static android.R.id.toggle;
+import static domain.hackathon.hackathon2017.R.id.home_back;
+
+public class InvaildPage extends AppCompatActivity {
+    private static final String TAG = "InvaildPage";
+    private FirebaseAuth.AuthStateListener mAuthListener;
+    private FirebaseDatabase mFirebaseDatabase;
+    private DatabaseReference myRef;
+    private FirebaseAuth mAuth = FirebaseAuth.getInstance();
+    FirebaseUser user = mAuth.getCurrentUser();
+    String USerid = user.getUid();
+    private EditText invalidMessage;
+    private DrawerLayout draw;
+    private ActionBarDrawerToggle toggle;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_invaild_page);
+        invalidMessage = (EditText) findViewById(R.id.invaildpagetxt);
+
+        invalidMessage.setText("Please Check Your Spelling");
+    }
+}
